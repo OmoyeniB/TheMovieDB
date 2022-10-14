95bbf83d09d457025435d2bd3a5d41918f330257
Omoyeni <babatundeabike3@gmail.com>
2022-10-14 11:44:41 +0100

    Create TVShowCollectionViewCell.swift

---
+++ MovieDB/MovieDB/AppView/View/HomePage/TVShowCell/TVShowCollectionViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  TVShowCollectionViewCell.swift
+//  MovieDB
+//
+//  Created by Sharon Omoyeni Babatunde on 13/10/2022.
+//
+
+import UIKit
+import Kingfisher
+
+
+class TVShowCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "TVShowCollectionViewCell"
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var releasedDate: UILabel!
+    @IBOutlet weak var movieDetails: UITextView!
+    @IBOutlet weak var movieRatings: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        initiateSetUp()
+    }
+    
+    func initiateSetUp() {
+        movieImage.setTopCornerRadius()
+        movieImage.addShadowToImageView()
+    }
+    
+    func setUpCell(with movieData: MovieList) {
+        self.downloadImage(with: movieData.imageURL, images: movieImage)
+        movieTitle.text = movieData.title
+        releasedDate.text = movieData.releaseDate
+        movieDetails.text = movieData.overview
+        movieRatings.setTitle(String(movieData.voteAverage ?? 0.0), for: .normal)
+    }
+    
+    @IBAction func ratingsAction(_ sender: Any) {
+    }
+    
+
+}
