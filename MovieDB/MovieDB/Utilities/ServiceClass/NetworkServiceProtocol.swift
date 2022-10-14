2ab9043365adcd432d3511f62ec5431cdb876cef
Omoyeni <babatundeabike3@gmail.com>
2022-10-14 22:29:34 +0100

    Update NetworkServiceProtocol.swift

--- MovieDB/MovieDB/Utilities/ServiceClass/NetworkServiceProtocol.swift
+++ MovieDB/MovieDB/Utilities/ServiceClass/NetworkServiceProtocol.swift
@@ -11,5 +11,7 @@ import Combine
 protocol MovieService {
     func networkRequest(from endpoint: ApiEndPointHandler) -> Future<[MovieList], NetworkingError>
     
-    func getUrlByEndPoint(with endpoint: ApiEndPointHandler) -> URL?
+    //    func networkRequest<T>(from endpoint: ApiEndPointHandler) -> Future<[T], NetworkingError>
+    
+//    func queryUrlComponents(urlComponents: URLComponents) -> URL?
 }
