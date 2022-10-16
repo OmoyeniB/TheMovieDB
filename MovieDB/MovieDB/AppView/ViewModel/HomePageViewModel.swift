81e1f141e1770411f42bf582d5d503b1abe3277d
Omoyeni <babatundeabike3@gmail.com>
2022-10-16 15:48:17 +0100

    Update HomePageViewModel.swift

--- MovieDB/MovieDB/AppView/ViewModel/HomePageViewModel.swift
+++ MovieDB/MovieDB/AppView/ViewModel/HomePageViewModel.swift
@@ -25,48 +25,48 @@ class HomePageViewModel {
     
     
     func getPopularMoviesCategories() {
-        NetworkManagerRepository.shared.networkRequest(from: .popular)
+        NetworkManagerRepository.shared.getMovieByCategorieEndpoints(from: .popular, id: nil, seasonNumber: nil, expecting: MovieData.self)
             .sink(receiveCompletion: { [unowned self] (completion) in
                 if case let .failure(error) = completion {
                     self.delegate?.errorNotifier(error)
                 }
-            }, receiveValue: { [unowned self] in self.popularMovieCategories = CombineDataHandler.populateItemFromServer(with: $0)
+            }, receiveValue: { [unowned self] in self.popularMovieCategories = CombineDataHandler.populateItemFromServer(with: $0.results)
                 getTopRatingMovieCategories()
             })
             .store(in: &self.subscriptions)
     }
     
     func getTopRatingMovieCategories() {
-        NetworkManagerRepository.shared.networkRequest(from: .top_rated)
+        NetworkManagerRepository.shared.getMovieByCategorieEndpoints(from: .top_rated, id: nil, seasonNumber: nil, expecting: MovieData.self)
             .sink(receiveCompletion: { [unowned self] (completion) in
                 if case let .failure(error) = completion {
                     self.delegate?.errorNotifier(error)
                 }
-            }, receiveValue: { [unowned self] in self.top_ratingMovieCategories = CombineDataHandler.populateItemFromServer(with: $0)
+            }, receiveValue: { [unowned self] in self.top_ratingMovieCategories = CombineDataHandler.populateItemFromServer(with: $0.results)
                 getAiring_todayMovieCategories()
             })
             .store(in: &self.subscriptions)
     }
     
     func getAiring_todayMovieCategories() {
-        NetworkManagerRepository.shared.networkRequest(from: .airing_today)
+        NetworkManagerRepository.shared.getMovieByCategorieEndpoints(from: .on_the_air, id: nil, seasonNumber: nil, expecting: MovieData.self)
             .sink(receiveCompletion: { [unowned self] (completion) in
                 if case let .failure(error) = completion {
                     self.delegate?.errorNotifier(error)
                 }
-            }, receiveValue: { [unowned self] in self.airing_todayMovieCategories = CombineDataHandler.populateItemFromServer(with: $0)
+            }, receiveValue: { [unowned self] in self.airing_todayMovieCategories = CombineDataHandler.populateItemFromServer(with: $0.results)
                 geton_the_airMovieCategories()
             })
             .store(in: &self.subscriptions)
     }
     
     func geton_the_airMovieCategories() {
-        NetworkManagerRepository.shared.networkRequest(from: .on_the_air)
+        NetworkManagerRepository.shared.getMovieByCategorieEndpoints(from: .airing_today, id: nil, seasonNumber: nil, expecting: MovieData.self)
             .sink(receiveCompletion: { [unowned self] (completion) in
                 if case let .failure(error) = completion {
                     self.delegate?.errorNotifier(error)
                 }
-            }, receiveValue: { [unowned self] in self.on_the_airMovieCategories = CombineDataHandler.populateItemFromServer(with: $0)
+            }, receiveValue: { [unowned self] in self.on_the_airMovieCategories = CombineDataHandler.populateItemFromServer(with: $0.results)
                 delegate?.configureUIAfterNetworkCall()
             })
             .store(in: &self.subscriptions)
