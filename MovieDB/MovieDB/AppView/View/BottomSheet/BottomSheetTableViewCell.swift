0f05d74161450d12dd083ec3fd52875ec543740d
Omoyeni <babatundeabike3@gmail.com>
2022-10-16 15:45:27 +0100

    Create BottomSheetTableViewCell.swift

---
+++ MovieDB/MovieDB/AppView/View/BottomSheet/BottomSheetTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileTableViewCell.swift
+//  MovieDB
+//
+//  Created by Sharon Omoyeni Babatunde on 16/10/2022.
+//
+
+import UIKit
+
+class BottomSheetTableViewCell: UITableViewCell {
+
+    static let identifier = "BottomSheet"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+ 
+
+}
