9dbe490d0191b6f016e4500d731cd48cc3ecd330
Omoyeni <babatundeabike3@gmail.com>
2022-10-16 09:28:22 +0100

    Create CombineDataHandler.swift

---
+++ MovieDB/MovieDB/MovieDB/Utilities/ServiceClass/CombineDataHandler.swift
@@ -0,0 +1,20 @@
+//
+//  CombineDataHandler.swift
+//  MovieDB
+//
+//  Created by Sharon Omoyeni Babatunde on 15/10/2022.
+//
+
+import Foundation
+
+final class CombineDataHandler {
+    
+   static func populateItemFromServer<T>(with movies: [T]) -> [T] {
+        return movies
+    }
+    
+    static func populateItemFromDetailServerQuery<T>(with movies: T) -> T {
+         return movies
+     }
+     
+}
