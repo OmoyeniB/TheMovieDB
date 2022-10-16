5b9d955f744ca7101a4c0a46268c1b479d993099
Omoyeni <babatundeabike3@gmail.com>
2022-10-16 09:23:31 +0100

    Create MovieEpisodeModel.swift

---
+++ MovieDB/MovieDB/MovieDB/AppView/Model/MovieEpisodeModel.swift
@@ -0,0 +1,24 @@
+//
+//  MovieEpisodeModel.swift
+//  MovieDB
+//
+//  Created by Sharon Omoyeni Babatunde on 15/10/2022.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct MovieEpisodeModel: Codable {
+    let id: Int?
+    let episodes: [Episode]
+    let name, overview: String?
+    let seasonNumber: Int?
+}
+
+// MARK: - Episode
+struct Episode: Codable {
+    let airDate: String?
+    let episodeNumber, id: Int?
+    let name: String?
+    let stillPath: String?
+}
