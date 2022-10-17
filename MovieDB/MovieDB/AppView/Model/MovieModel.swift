b913af50f68277fe83d423e965435bb176f288d1
Omoyeni <babatundeabike3@gmail.com>
2022-10-17 05:16:05 +0100

    Update MovieModel.swift

--- MovieDB/MovieDB/AppView/Model/MovieModel.swift
+++ MovieDB/MovieDB/AppView/Model/MovieModel.swift
@@ -15,7 +15,7 @@ struct MovieData: Codable {
 }
 
 // MARK: - Result
-struct MovieList: Codable, Equatable, Hashable {
+struct MovieList: Codable {
     let backdropPath: String?
     let genreIDS: [Int]?
     let id: Int?
@@ -25,7 +25,7 @@ struct MovieList: Codable, Equatable, Hashable {
     let releaseDate, title: String?
     let voteAverage: Double?
     let poster_path: String?
-    let first_air_date: String?
+    let firstAirDate: String?
     let name: String?
     let vote_average: String?
    
@@ -33,21 +33,3 @@ struct MovieList: Codable, Equatable, Hashable {
         return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath ?? poster_path ?? "")")!
     }
 }
-
-
-enum OriginalLanguage {
-    case en
-    case fr
-    case ja
-}
-
-//struct TVList: Codable {
-//    let poster_path: String?
-//    let first_air_date: String?
-//    let name: String?
-//    let overview: String?
-//    let vote_average: String?
-//    let id: Int?
-//    let voteAverage: Double?
-//}
-
